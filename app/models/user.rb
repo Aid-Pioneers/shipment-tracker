6d05ada170533949f1fcfc2cafb6d987c46c6c52
Vincent Stuber <vincentstuber97@googlemail.com>
2022-05-31 13:44:30 +0200

    removing email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,6 @@ class User < ApplicationRecord
   has_many :donations, class_name: "Pallet", foreign_key: "user_id"
   has_many :shipments
 
-  validates :email,
-            format: { with: /\\A(.+)@(.+)\\z/, message: "Email invalid" },
-            uniqueness: { case_sensitive: false },
-            length: { minimum: 4, maximum: 254 }
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
