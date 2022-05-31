b4ed5c51eb8df6f5e47527c576fe478aff3caac4
Vincent Stuber <vincentstuber97@googlemail.com>
2022-05-31 17:11:43 +0200

    adding pallet new and create

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,8 @@ class User < ApplicationRecord
             length: { minimum: 4, maximum: 254 }
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
