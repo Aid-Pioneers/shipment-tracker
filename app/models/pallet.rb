ae8ee302cc19cc8cc2b391bd3882a4e4d35201e8
sophie sorin <sophie4489@hotmail.fr>
2022-06-01 17:35:11 +0200

    didnt change anything please ignore

--- app/models/pallet.rb
+++ app/models/pallet.rb
@@ -1,5 +1,5 @@
 class Pallet < ApplicationRecord
-  CATEGORIES = ["Food", "Medicine", "Hygiene", "Clothes"]
+  CATEGORIES = ["Food", "Medicine", "Hygiene", "Clothes", ]
 
   belongs_to :donor, class_name: "User", foreign_key: "user_id"
   belongs_to :shipment
