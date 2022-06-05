66c2dad1931515e3ceb6fd12af04f75070f68236
Vincent Stuber <vincentstuber97@googlemail.com>
2022-06-05 19:14:06 +0200

    adding map to donations page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,13 @@ class PagesController < ApplicationController
 
   def donations
     @shipments = current_user.donations.includes(shipment: :scans).order(shipment_id: :desc).map(&:shipment).uniq
+    scans = @shipments.select { |shipment| shipment.scans.size.positive? }.map { |shipment| shipment.scans.first }
+    @markers = scans.map do |scan|
+      {
+        lat: scan.latitude,
+        lng: scan.longitude
+      }
+    end
     @donations = current_user.donations
   end
 end
