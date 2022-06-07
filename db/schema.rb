0a2df65d8519fc24127c95036cd3e7d2e195b190
finn stuerenburg <finnstueren@gmail.com>
2022-06-07 10:10:15 +0200

    added 4 columns to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_142626) do
+ActiveRecord::Schema.define(version: 2022_06_07_080723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,10 @@ ActiveRecord::Schema.define(version: 2022_06_02_142626) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0
+    t.string "start_lat"
+    t.string "start_lon"
+    t.string "destination_lat"
+    t.string "destination_lon"
     t.index ["project_id"], name: "index_shipments_on_project_id"
     t.index ["user_id"], name: "index_shipments_on_user_id"
   end
