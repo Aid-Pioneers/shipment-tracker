58f8ca867f7107cb1f0bc5c3bcda9c036826a9e9
finn stuerenburg <finnstueren@gmail.com>
2022-06-07 09:25:26 +0200

    changed the style of the map

--- app/javascript/controllers/mapbox_show_controller.js
+++ app/javascript/controllers/mapbox_show_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/starshipsssss/cl42fzh30002x14qlx9knq50y"
       // interactive: false
     })
 
