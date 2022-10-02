5d11050cafa1a15948e08d6cdf3c5d59023a60c6
nenko.tabakov <nenko.tabakov@gmail.com>
2022-10-02 12:47:08 +0100

    feat: added endpoint for creating project that accessible by admins only

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,8 +1,20 @@
 class ProjectPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      if user.donor?
+        scope.where(user: user)
+      else
+        scope.all
+      end
+    end
+  end
+
+  def new?
+    user.admin?
+  end
+
+  def create?
+    user.admin?
   end
 end
