d4118f2274c5f10f525fdf38ec986ef6bc0305e6
Finn Stuerenburg <finnstueren@gmail.com>
2023-04-21 19:19:54 +0200

    Fix: Delete the option to the QR types

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -6,7 +6,6 @@ class Shipment < ApplicationRecord
   has_many :pallets
 
   enum transport_type: [:truck, :train, :plane, :ship]
-  enum qr_code_type: [:authentic_network, :regular]
   enum status: [:in_preparation, :in_transit, :delivered, :problematic]
 
   validates :qr_code_type, presence: true
